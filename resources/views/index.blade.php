b4d10861451e417927ee0cf47e572d1d6c6e1a3f
Macsback <119421402+Macsback@users.noreply.github.com>
2024-03-08 10:04:49 +0000

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class=" grid grid-cols-1 m-auto">
+    <div class=" background-image grid grid-cols-1 m-auto">
+    <div class="bg-black bg-opacity-50">
         <div class="flex text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
-                <img src= >
+        
                 <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-9">
                    the mind of a cat
                         
@@ -20,50 +21,56 @@
             </div>
         </div>
     </div>
+    </div>
 
     <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
         <div>
-            <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700" alt="">
+            <img src="images/Cat-bg.jpeg" width="700" alt="">
         </div>
 
         <div class="m-auto sm:m-auto text-left w-4/5 block">
             <h2 class="text-3xl font-extrabold text-gray-600">
-                Struggling to be a better web developer?
+               Who am I?
             </h2>
             
             <p class="py-8 text-gray-500 text-s">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.
+                I'm Dr. Fuzzypaws, I have a PHD on Cat Behaviour and Purr science from the university of Meowssachusetts.
+                I've dedicated my whole life to studying my fellow catmates and their behaviour. And Ive Realized how little 
+                the common purrpulation knows about the profoundness of our specie's minds.
             </p>
 
             <p class="font-extrabold text-gray-600 text-s pb-9">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente magnam vero nostrum! Perferendis eos molestias porro vero. Vel alias.
+            So I've decided to share my life work with you
+             on this blog.
             </p>
 
             <a 
                 href="/blog"
-                class="uppercase bg-blue-500 text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl">
+                class="uppercase bg-green-300 text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl">
                 Find Out More
             </a>
         </div>
     </div>
 
-    <div class="text-center p-15 bg-black text-white">
+    <div class="text-center p-15 bg-green-600 text-white">
         <h2 class="text-2xl pb-5 text-l"> 
-            I'm an expert in...
+            What you'll learn about in this blog
         </h2>
 
         <span class="font-extrabold block text-4xl py-1">
-            Ux Design
+            Cat behavior
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Project Management
+            Dog - Cat Relationships
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Digital Strategy
+            Napping
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Backend Development
+            Zoomies
         </span>
+
+     
     </div>
 
     <div class="text-center py-15">
