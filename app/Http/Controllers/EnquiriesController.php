0ebfbf66f93c7e3fe97044c896258f0a1251379c
Macsback <119421402+Macsback@users.noreply.github.com>
2024-03-11 15:38:41 +0000

    Working on new blog page

--- app/Http/Controllers/EnquiriesController.php
+++ app/Http/Controllers/EnquiriesController.php
@@ -11,4 +11,21 @@ class EnquiriesController extends Controller
         $data['enquieries'] = Enquiry::orderby('id')->paginate(10);
         return view('enquiries.index', $data);
     }
+
+    public function create() {
+        return view('enquiries.create');
+    }
+
+    public function store(Request $request) {
+        $enquiry = new Enquiry();
+        $enquiry->title = $request->title;
+        $enquiry->email = $request->email;
+        $enquiry->message = $request->message;
+        $enquiry->is_urgent = $request->has('is_urgent');
+        $enquiry->save();
+
+        return redirect()->route('enquiries.index')
+        ->with('success', 'Enquiry created successfully' );
+    }
+
 }
